6dc0f1b6920b29a3a6832b98cd1e65ce679bdd15
Darina Pavlova <pavlovdarina@gmail.com>
2023-10-03 20:05:05 +0300

    Аdd @BeforeMethod in BaseTestSetup class

--- Rest Assured Testing - WeAre Project/src/test/java/base/BaseTestSetup.java
+++ Rest Assured Testing - WeAre Project/src/test/java/base/BaseTestSetup.java
@@ -1,8 +1,12 @@
 package base;
 
+import io.restassured.RestAssured;
 import io.restassured.http.Cookies;
 import io.restassured.response.Response;
+import org.testng.annotations.BeforeMethod;
 
+import static Utils.Endpoints.AUTHENTICATE_ENDPOINT;
+import static Utils.Endpoints.BASE_URL;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.testng.Assert.assertEquals;
 
@@ -10,13 +14,30 @@ public class BaseTestSetup {
 
     public static Cookies cookies;
 
-
     public static void AssertResponse(Response response) {
         int statusCode = response.getStatusCode();
         assertEquals(statusCode, SC_OK, "Incorrect status code.");
         if (statusCode == SC_OK) {
             System.out.println("Test passed.");
         }
+    }
+
+    @BeforeMethod
+
+    public void authenticateAndFetchCookies() {
+        RestAssured.baseURI = BASE_URL;
+
+        Response response = RestAssured.given()
+                .contentType("multipart/form-data")
+                .multiPart("username", "Grandmama")
+                .multiPart("password", "Project.10")
+                .when()
+                .post(AUTHENTICATE_ENDPOINT);
+
+        cookies = response.detailedCookies();
+        int statusCodeAuthentication = response.getStatusCode();
+        System.out.println("The status code is:" + statusCodeAuthentication);
+
     }
 
 }
--- Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/UpgradeUserPersonalProfile.java
+++ Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/UpgradeUserPersonalProfile.java
@@ -12,27 +12,11 @@ import static Utils.Endpoints.BASE_URL;
 import static Utils.JSONRequests.UPGRADE_USER_PERSONAL_PROFILE_BODY;
 
 public class UpgradeUserPersonalProfile extends BaseTestSetup {
-    private Cookies cookies;
 
-    @BeforeMethod
-
-    public void authenticateAndFetchCookies() {
-        RestAssured.baseURI = BASE_URL;
-
-        Response response = RestAssured.given()
-                .contentType("multipart/form-data")
-                .multiPart("username", "Grandmama")
-                .multiPart("password", "Project.10")
-                .when()
-                .post(AUTHENTICATE_ENDPOINT);
-
-        cookies = response.detailedCookies();
-        int statusCodeAuthentication = response.getStatusCode();
-        System.out.println(statusCodeAuthentication);
-    }
 
     @Test
     public void updateUserPersonalProfileTest() {
+
         RestAssured.baseURI = BASE_URL;
 
         Response response = RestAssured.given()
