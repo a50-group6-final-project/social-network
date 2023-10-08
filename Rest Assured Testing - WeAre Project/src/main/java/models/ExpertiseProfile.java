40361ce256dbb7363561836299b9b4b7e8384452
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-08 10:33:37 +0300

    add UserProfile, Page, ExpertiseProfile models

---
+++ Rest Assured Testing - WeAre Project/src/main/java/models/ExpertiseProfile.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+
+public class ExpertiseProfile {
+    public int id;
+    public ArrayList<Skill> skills;
+    public Category category;
+    public double availability;
+}
