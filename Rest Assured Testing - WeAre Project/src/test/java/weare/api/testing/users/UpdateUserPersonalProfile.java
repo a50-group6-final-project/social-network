2c11d4fb2e0f9d894e3f08b5117270628ede20ec
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-09 17:47:10 +0300

    update UpdateUserPersonalProfile

--- Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/users/UpdateUserPersonalProfile.java
+++ Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/users/UpdateUserPersonalProfile.java
@@ -3,23 +3,33 @@ package weare.api.testing.users;
 import Utils.Serializer;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import models.City;
 import models.UserPersonal;
+import models.UserUpdatePersonal;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static Utils.Endpoints.BASE_URL;
 
 public class UpdateUserPersonalProfile extends BaseUserSetup {
 
 
     @Test
     public void updateUserPersonalProfile_Successful() {
-        currentUserPersonalProfile.firstName = "firstTestUpdated";
-        currentUserPersonalProfile.lastNAme = "lastTestUpdated";
-        currentUserPersonalProfile.gender = "MALE";
-        currentUserPersonalProfile.city = "Sofia";
-        currentUserPersonalProfile.birthYear = "1990";
-        String bodyUpdatedPersonalProfileString = Serializer.convertObjectToJsonString(currentUserPersonalProfile);
+        UserUpdatePersonal updateProfile = new UserUpdatePersonal();
+        updateProfile.username = currentUserPersonalProfile.username;
+        updateProfile.email = currentUserPersonalProfile.email;
+        updateProfile.firstName = "firstName Updated";
+        updateProfile.lastName = "lastName Updated";
+        updateProfile.location.city = new City();
+        updateProfile.location.city.id = 1;
+        updateProfile.birthYear = "1990-04-04";
+        updateProfile.skills = currentUserPersonalProfile.skills;
+
+        String bodyUpdatedPersonalProfileString = Serializer.convertObjectToJsonString(updateProfile);
 
         Response response = RestAssured.given()
+                .baseUri(BASE_URL)
                 .cookie("JSESSIONID", JSESSIONID)
                 .contentType("application/json")
                 .body(bodyUpdatedPersonalProfileString)
