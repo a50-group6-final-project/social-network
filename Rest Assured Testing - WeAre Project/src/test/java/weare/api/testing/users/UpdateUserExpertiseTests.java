f091df3307beaefbbbf4f165485306c7d6cdd0a0
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-09 16:53:25 +0300

    update and format UpdateUserExpertiseTests

--- Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/users/UpdateUserExpertiseTests.java
+++ Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/users/UpdateUserExpertiseTests.java
@@ -1,12 +1,16 @@
 package weare.api.testing.users;
 
 import Utils.Serializer;
-import base.BaseTestSetup;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.testng.annotations.BeforeClass;
+import models.Category;
+import models.ExpertiseProfile;
+import models.Skill;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import weare.api.testing.skills.CreateSkillTests;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static Utils.Endpoints.BASE_URL;
 import static Utils.Endpoints.UPDATE_ENDPOINT;
@@ -15,13 +19,12 @@ public class UpdateUserExpertiseTests extends BaseUserSetup {
 
     @Test
     public void updateUserExpertise_Successful() {
-        System.out.println("STOP");
-        //update currentUserProfile.expertiseProfile
-        currentUserProfile.expertiseProfile.category.id = 157;
-        currentUserProfile.expertiseProfile.category.name = "Marketing Updated";
-        currentUserProfile.expertiseProfile.availability = 33.66;
+        int categoryId = 157;
+        String categoryName = "Marketing Updated";
+        double availability = 33.66;
+        ExpertiseProfile update = updateExpertiseProfile(categoryId, categoryName, availability);
 
-        String bodyExpertiseProfileString = Serializer.convertObjectToJsonString(currentUserProfile.expertiseProfile);
+        String bodyExpertiseProfileString = Serializer.convertObjectToJsonString(update);
 
         Response response = RestAssured.given()
                 .baseUri(BASE_URL)
@@ -29,11 +32,35 @@ public class UpdateUserExpertiseTests extends BaseUserSetup {
                 .cookie("JSESSIONID", getJSESSIONIDCookie(currentUsername, "Project.10"))
                 .body(bodyExpertiseProfileString)
                 .when()
-//                .put("/api/users/expertise/" + currentUserId);
                 .post(UPDATE_ENDPOINT + currentUserId + "/expertise");
 
-            System.out.println(response.asString());
-            isResponse200(response);
+        isResponse200(response);
 
+
+        currentUserProfile.expertiseProfile = response.as(ExpertiseProfile.class);
+        Assert.assertEquals(currentUserProfile.expertiseProfile.category.id, categoryId, "Category id is not updated!");
+        Assert.assertEquals(currentUserProfile.expertiseProfile.category.name, categoryName, "Category name is not updated!");
+        Assert.assertEquals(currentUserProfile.expertiseProfile.availability, availability, "Availability is not updated!");
+    }
+
+
+    private ExpertiseProfile updateExpertiseProfile(int categoryId, String categoryName, double availability, Skill[] skills) {
+        ExpertiseProfile expertiseProfile = new ExpertiseProfile();
+        expertiseProfile.category = new Category();
+        expertiseProfile.category.id = categoryId;
+        expertiseProfile.category.name = categoryName;
+        expertiseProfile.availability = availability;
+        expertiseProfile.skills.addAll(List.of(skills));
+
+        return expertiseProfile;
+    }
+    private ExpertiseProfile updateExpertiseProfile(int categoryId, String categoryName, double availability) {
+        ExpertiseProfile expertiseProfile = new ExpertiseProfile();
+        expertiseProfile.category = new Category();
+        expertiseProfile.category.id = categoryId;
+        expertiseProfile.category.name = categoryName;
+        expertiseProfile.availability = availability;
+
+        return expertiseProfile;
     }
 }
