a5157edcc06b75a441265393282b5b9a31d68e2d
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-12 15:04:17 +0300

    add files

---
+++ Rest Assured Testing - WeAre Project/src/main/java/api/UserController.java
@@ -0,0 +1,12 @@
+package api;
+
+import models.UserRegister;
+
+public class UserController {
+
+    public static void createUser(UserRegister userToRegister) {
+    }
+
+    public static void authenticateUser(UserRegister userToRegister) {
+    }
+}
