100fbf59c9481e97a1ed93b13340c0bd79bcccc1
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-12 18:32:27 +0300

    add PostController.editPost

--- Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/post/EditPostTest.java
+++ Rest Assured Testing - WeAre Project/src/test/java/weare/api/testing/post/EditPostTest.java
@@ -1,45 +1,55 @@
 package weare.api.testing.post;
 
-import Utils.Serializer;
+import Utils.ModelGenerator;
+import api.PostController;
 import base.BaseTestSetup;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import models.EditPost;
 import models.PostModel;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static Utils.Endpoints.*;
-import static org.testng.Assert.assertEquals;
 
 public class EditPostTest extends BaseTestSetup {
     @BeforeTest
-    public void createPost_Successful() {
-        String uniqueContent = generateUniqueContentPost();
+    public void setup() {
+        if (!isRegistered) {
+            postCreatorUsername = generateUniqueUsername();
+            currentEmail = generateUniqueEmail();
+            register(postCreatorUsername, currentEmail);
+            authenticateAndFetchCookies(postCreatorUsername, "Project.10");
+            isRegistered = true;
+        }
 
+        String uniqueContent = generateUniqueContentPost();
+        createPost = ModelGenerator.generatePostModel(uniqueContent);
+        Response response = PostController.createPost(cookies, createPost);
+        createdPost = response.as(PostModel.class);
+        System.out.println("Successfully created a new post with Id" + " " + createdPost.postId);
     }
 
     @Test
     public void editPost_Successful() {
-
-        editPost.content="I am looking for a painter";
-
-        String bodyEditPostString = Serializer.convertObjectToJsonString(editPost);
-
-
-        Response response = RestAssured.given()
-                .cookies(cookies)
-                .contentType("application/json")
-                .queryParam("postId", postId)
-                .body(bodyEditPostString)
-                .when()
-                .put(EDIT_POST_ENDPOINT);
-
-
+        createdPost.content = "I am looking for a painter";
+        Response response = PostController.editPost(cookies, createdPost);
         isResponse200(response);
-//        Assert.assertNotNull(editPost);
+
+//        editPost = response.as(PostModel.class);
+
+        //Add some assertions
+//        Assert.assertEquals(editPost.content, createdPost.content, "Content does not match.");
+
+        System.out.println("Post with Id" + " " + createdPost.postId + " " + "edited successfully.");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        Response response = PostController.deletePost(cookies, createdPost.postId);
+        isResponse200(response);
         System.out.println(response.asString());
-        System.out.println("Post with Id" + " " + postId + " " + "edited successfully.");
+        System.out.println("Post with Id" + " " + postId + " " + "Deleted successfully.");
     }
 }
