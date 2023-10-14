211233dd656c985766f82c09d61c76ea9bbc5972
Darina Pavlova <pavlovdarina@gmail.com>
2023-10-14 11:55:42 +0300

    Adding tests in RegisterTest

--- SeleniumWebDrive_WeAreProject/src/test/java/weare/testing/RegisterTest.java
+++ SeleniumWebDrive_WeAreProject/src/test/java/weare/testing/RegisterTest.java
@@ -1,30 +1,50 @@
 package weare.testing;
 
+import com.telerikacademy.pages.BasePage;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
-import static weare.testing.RegisterPage.*;
 
 public class RegisterTest extends BaseTestSetup {
 
     @Test
     public void userRegisterWhenInputValidData() {
+        BaseTestSetup.generatedUsername = BaseTestSetup.generateRandomUsername(6);
+        BaseTestSetup.generatedPassword = BaseTestSetup.generateRandomPassword();
+        BaseTestSetup.generatedEmail = BaseTestSetup.generateRandomEmail();
 
-       registerNewUser(8);
+        registerPage.userRegister(BaseTestSetup.generatedUsername, BaseTestSetup.generatedPassword, BaseTestSetup.generatedEmail);
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-
-        registerPage.assertElementPresent("//a[@id='button']");
+        BasePage.assertElementPresent("//a[@id='button']");
     }
-
-
 
     @Test
-    public void userRegisterWhenInputUserNameWithMinLength() {
-        registerNewUser(2);
+    public void userRegisterSuccessfullyWhenInputUsernameWithMinLength() {
+        BaseTestSetup.generatedUsername = BaseTestSetup.generateRandomUsername(2);
+        BaseTestSetup.generatedPassword = BaseTestSetup.generateRandomPassword();
+        BaseTestSetup.generatedEmail = BaseTestSetup.generateRandomEmail();
+
+        registerPage.userRegister(BaseTestSetup.generatedUsername, BaseTestSetup.generatedPassword, BaseTestSetup.generatedEmail);
+
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        BasePage.assertElementPresent("//a[@id='button']");
+    }
 
-        registerPage.assertElementPresent("//a[@id='button']");
+    @Test
+    public void userRegisterSuccessfullyWhenInputUsernameWithMaxLength() {
+        BaseTestSetup.generatedUsername = BaseTestSetup.generateRandomUsername(20);
+        BaseTestSetup.generatedPassword = BaseTestSetup.generateRandomPassword();
+        BaseTestSetup.generatedEmail = BaseTestSetup.generateRandomEmail();
+
+        registerPage.userRegister(BaseTestSetup.generatedUsername, BaseTestSetup.generatedPassword, BaseTestSetup.generatedEmail);
+
+
+        BasePage.assertElementPresent("//a[@id='button']");
     }
 }
+
