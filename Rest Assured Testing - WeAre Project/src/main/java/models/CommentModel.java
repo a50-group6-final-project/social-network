e33dd55f32dd70848090ad8cea99f6c5a635a08a
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-14 11:16:08 +0300

    update CommentModel

--- Rest Assured Testing - WeAre Project/src/main/java/models/CommentModel.java
+++ Rest Assured Testing - WeAre Project/src/main/java/models/CommentModel.java
@@ -8,8 +8,8 @@ public class CommentModel {
     public boolean deletedConfirmed;
     public int postId;
     public int userId;
-
     public String date;
     public ArrayList<Object> likes;
     public boolean liked;
+    public Object hibernateLazyInitializer;
 }
