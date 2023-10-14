4532b12057b69a560197eb16f1292823f2b68982
Darina Pavlova <pavlovdarina@gmail.com>
2023-10-14 17:51:11 +0300

    Adding AdminPage

---
+++ SeleniumWebDrive_WeAreProject/src/test/java/weare/testing/AdministrativePartTests.java
@@ -0,0 +1,22 @@
+package weare.testing;
+
+import com.telerikacademy.pages.BasePage;
+import org.junit.jupiter.api.Test;
+
+import static weare.testing.BaseTestSetup.registerPage;
+
+public class AdministrativePartTests extends BaseTestSetup{
+
+    @Test
+    public void  AdminRegistered_When_InputValidData() {
+        String username = BaseTestSetup.generateRandomUsernameWithAdmin(8);
+        String password = BaseTestSetup.generateRandomPassword(10);
+        String email = BaseTestSetup.generateRandomEmail();
+
+        registerPage.adminRegister(username, password, email);
+
+        LoginPage.loginUser(username,password);
+
+        BasePage.assertElementPresent("//a[normalize-space(text())='GO TO admin zone']");
+    }
+}
