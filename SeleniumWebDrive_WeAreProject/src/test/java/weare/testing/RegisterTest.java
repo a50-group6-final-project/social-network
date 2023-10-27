2ea8d7969ce919e6fd34654d517dc20412500386
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-10-27 15:22:43 +0300

    add Allure Report

--- SeleniumWebDrive_WeAreProject/src/test/java/weare/testing/RegisterTest.java
+++ SeleniumWebDrive_WeAreProject/src/test/java/weare/testing/RegisterTest.java
@@ -42,68 +42,6 @@ public class RegisterTest extends BaseTestSetup {
     }
 
     @Test
-    public void UserNotRegistered_When_TryToInputUsernameWithOneSymbol() {
-        String username = BaseTestSetup.generateRandomUsername(1);
-        String password = BaseTestSetup.generateRandomPassword(12);
-        String email = BaseTestSetup.generateRandomEmail();
-
-
-        registerPage.userRegister(username, password, email);
-
-        BasePage.assertErrorPresent("weAre.basePage.whitelabelError", "User is registered with an invalid number of password characters");
-    }
-
-    @Test
-    public void UserNotRegistered_When_TryToInputUsernameExceedingLimit() {
-        String username = BaseTestSetup.generateRandomUsername(21);
-        String password = BaseTestSetup.generateRandomPassword(12);
-        String email = BaseTestSetup.generateRandomEmail();
-
-
-        registerPage.userRegister(username, password, email);
-
-
-        BasePage.assertErrorPresent("weAre.basePage.whitelabelError", "User is registered with an invalid number of username characters");
-
-    }
-
-    @Test
-    public void UserNotRegistered_When_TryToInputPasswordWithThreeCharacters() {
-        String username = BaseTestSetup.generateRandomUsername(8);
-        String password = BaseTestSetup.generateRandomPassword(3);
-        String email = BaseTestSetup.generateRandomEmail();
-
-        registerPage.userRegister(username, password, email);
-        BasePage.assertErrorPresent("weAre.basePage.passwordError", "User is registered with an invalid number of password characters");
-
-    }
-
-    @Test
-    public void UserNotRegistered_When_TryToInputPasswordWithSevenCharacters() {
-        String username = BaseTestSetup.generateRandomUsername(7);
-        String password = BaseTestSetup.generateRandomPassword(7);
-        String email = BaseTestSetup.generateRandomEmail();
-
-
-        registerPage.userRegister(username, password, email);
-
-        BasePage.assertErrorPresent("weAre.basePage.passwordError", "User is registered with an invalid number of password characters");
-
-    }
-
-    @Test
-    public void UserNotRegistered_When_TryToInputPasswordWithLettersOnly() {
-        String username = BaseTestSetup.generateRandomUsername(7);
-        String letterPassword = BaseTestSetup.generateLetterPassword(10);
-        String email = BaseTestSetup.generateRandomEmail();
-
-        registerPage.userRegister(username, letterPassword, email);
-
-        BasePage.assertErrorPresent("weAre.basePage.whitelabelError", "User can be registered with a password consisting only of letters");
-
-    }
-
-    @Test
     public void UserRegisteredSuccessfully_When_SelectProfessionalCategory() {
         String username = BaseTestSetup.generateRandomUsername(6);
         String password = BaseTestSetup.generateRandomPassword(10);
